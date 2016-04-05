19cb342c5c6b681226707e99bc0dad34d23ae436
Oliver <iosdev@ety-idev-mba-01.local>
2016-04-05 10:10:26 +0300

    First Commit

--- ClownFiesta/ViewController.swift
+++ ClownFiesta/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var teamButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        teamButton.layer.borderWidth = 1
+        teamButton.layer.borderColor = UIColor.blackColor().CGColor
+    }
+    
+    // MARK: Actions
+    @IBAction func teamButton(sender: UIButton) {
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    // Disable screen orientation
+    override func shouldAutorotate() -> Bool {
+        return false
+    }
+    
+    override func supportedInterfaceOrientations() -> UIInterfaceOrientationMask {
+        return UIInterfaceOrientationMask.Portrait
+    }
 }
 
