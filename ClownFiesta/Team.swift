531f007d1f06509e0b5b293057342d5bb2c981d1
Oliver <iosdev@ety-idev-mba-01.local>
2016-04-07 11:01:39 +0300

    Core Data and update to Storyboard

---
+++ ClownFiesta/Team.swift
@@ -0,0 +1,18 @@
+//
+//  Team.swift
+//  ClownFiesta
+//
+//  Created by iosdev on 7.4.2016.
+//  Copyright © 2016 Oliver. All rights reserved.
+//
+
+
+import UIKit
+import CoreData
+import Foundation
+
+class Team: NSManagedObject {
+    
+    @NSManaged var name: String?
+    
+}
